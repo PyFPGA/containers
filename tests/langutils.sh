d14befe1753460f82e6e42dad831d513cb495bf6
Rodrigo A. Melo <rodrigomelo9@gmail.com>
2024-12-04 20:51:51 -0300

    Add sv2v into the langutils recipe

--- tests/langutils.sh
+++ tests/langutils.sh
@@ -4,3 +4,4 @@ set -e
 bash ../run.sh langutils 'vhd2vl --version'
 bash ../run.sh langutils 'slang --version'
 bash ../run.sh langutils 'surelog --version'
+bash ../run.sh langutils 'sv2v --version'
