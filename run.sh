52d4673f3edf9eed6f22f81acb12f687002762f5
Rodrigo A. Melo <rodrigomelo9@gmail.com>
2024-12-06 22:21:34 -0300

    Modified to run as a normal user

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 set -e
 IMAGE=$1
 COMMAND=$2
-docker run --rm -v $PWD:$PWD -w $PWD ghcr.io/pyfpga/$IMAGE $COMMAND
+docker run --user $(id -u):$(id -g) --rm -v $PWD:$PWD -w $PWD ghcr.io/pyfpga/$IMAGE $COMMAND
