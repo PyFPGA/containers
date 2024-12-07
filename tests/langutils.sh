cbfb858567f3e1760c7d04b2fd68aa27cffa8f24
Rodrigo A. Melo <rodrigomelo9@gmail.com>
2024-12-06 23:54:17 -0300

    langutils: add tests (analysis and generation)

--- tests/langutils.sh
+++ tests/langutils.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
+
 set -e
 
-bash ../run.sh langutils 'vhd2vl --version'
-bash ../run.sh langutils 'slang --version'
-bash ../run.sh langutils 'surelog --version'
-bash ../run.sh langutils 'sv2v --version'
-bash ../run.sh langutils 'verible-verilog-syntax --version'
+DOCKER="docker run --rm -v $HOME:$HOME -w $PWD --user $(id -u):$(id -g) ghcr.io/pyfpga/langutils"
+
+mkdir -p results
+
+$DOCKER vhd2vl --version
+$DOCKER slang --version
+$DOCKER surelog --version
+$DOCKER sv2v --version
+$DOCKER verible-verilog-syntax --version
+
+$DOCKER vhd2vl --quiet hdl/counter.vhdl results/vhd2vl.v
+$DOCKER slang hdl/counter.sv --lint-only
+$DOCKER surelog -parse hdl/counter.sv
+$DOCKER sv2v hdl/counter.sv --write=results/sv2v.v
+$DOCKER verible-verilog-lint hdl/counter.sv
+
+rm -fr slpp_all
