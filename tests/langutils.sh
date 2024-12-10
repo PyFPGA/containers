e38e714c88e1ca12be4ed823704e70d275fc0db7
Rodrigo A. Melo <rodrigomelo9@gmail.com>
2024-12-10 18:39:31 -0300

    langutils: temporarily removed verible which is experimenting an issue with Bazel

--- tests/langutils.sh
+++ tests/langutils.sh
@@ -10,12 +10,12 @@ $DOCKER vhd2vl --version
 $DOCKER slang --version
 $DOCKER surelog --version
 $DOCKER sv2v --version
-$DOCKER verible-verilog-syntax --version
+# $DOCKER verible-verilog-syntax --version
 
 $DOCKER vhd2vl --quiet hdl/counter.vhdl results/vhd2vl.v
 $DOCKER slang hdl/counter.sv --lint-only
 $DOCKER surelog -parse hdl/counter.sv
 $DOCKER sv2v hdl/counter.sv --write=results/sv2v.v
-$DOCKER verible-verilog-lint hdl/counter.sv
+# $DOCKER verible-verilog-lint hdl/counter.sv
 
 rm -fr slpp_all
