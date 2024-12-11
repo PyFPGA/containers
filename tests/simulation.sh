410a72ff01972f27259794017966531ceb5e7652
Rodrigo A. Melo <rodrigomelo9@gmail.com>
2024-12-10 22:42:00 -0300

    Add tests for simulation

---
+++ tests/simulation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+DOCKER="docker run --rm -v $HOME:$HOME -w $PWD --user $(id -u):$(id -g) ghcr.io/pyfpga/simulation"
+
+$DOCKER ghdl --version
+$DOCKER iverilog -V | grep Icarus
+$DOCKER verilator --version
