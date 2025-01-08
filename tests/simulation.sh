841b594feaef732b290b024a2984380c178a37c7
Rodrigo A. Melo <rodrigomelo9@gmail.com>
2025-01-07 21:37:29 -0300

    simulation: add cocotb

--- tests/simulation.sh
+++ tests/simulation.sh
@@ -7,3 +7,4 @@ DOCKER="docker run --rm -v $HOME:$HOME -w $PWD --user $(id -u):$(id -g) ghcr.io/
 $DOCKER ghdl --version
 $DOCKER iverilog -V | grep Icarus
 $DOCKER verilator --version
+$DOCKER cocotb-config --version
